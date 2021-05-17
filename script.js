13b638020647afee82b115d563b52b5d62bae7a1
pripac <81447927+pripac@users.noreply.github.com>
2021-05-17 13:46:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// programa-se 1o o conteudo que quer ter na tela e se preciso coloca mais tags pra estilizar p/ deixar o html sempre o mais limpo possível
+
+//div e section vem dentro "aninhado" no section, é uma estrutura de camadas
+
+// a imagem é um elemento de conteúdo, por isso é preciso codar display: block para transformar em conteúdo de bloco
